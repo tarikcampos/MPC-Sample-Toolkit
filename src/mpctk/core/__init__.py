df3a872d81decd2bbfc2b5d17625b515eaf55f95
tarikcampos <tarik.campos@gmail.com>
2026-08-18 11:00:59 +0200

    Add core package initializer

---
+++ src/mpctk/core/__init__.py
@@ -0,0 +1,7 @@
+"""
+Core functionality for MPC Sample Toolkit.
+
+This package contains the low-level building blocks used by MPCTK,
+including compression utilities, XPJ parsing, project models, and
+project writing.
+"""
